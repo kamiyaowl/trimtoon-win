9eb57485b76dbbfdc36b1ba2b8b109139ee63c7c
K <fieldprogrammable@gmail.com>
2018-12-15 20:30:17 +0900

    graph.Importできた

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,22 +27,9 @@ namespace trimtoon_win
         }
 
         private void Grid_Loaded(object sender, RoutedEventArgs e) {
-            using (var session = new TFSession()) {
-                var graph = session.Graph;
-
-                var a = graph.Const(2);
-                var b = graph.Const(3);
-                Console.WriteLine("a=2 b=3");
-
-                // Add two constants
-                var addingResults = session.GetRunner().Run(graph.Add(a, b));
-                var addingResultValue = addingResults.GetValue();
-                Console.WriteLine("a+b={0}", addingResultValue);
-
-                // Multiply two constants
-                var multiplyResults = session.GetRunner().Run(graph.Mul(a, b));
-                var multiplyResultValue = multiplyResults.GetValue();
-                Console.WriteLine("a*b={0}", multiplyResultValue);
+            using (var graph = new TFGraph()) {
+                graph.Import(File.ReadAllBytes("tf_model/model.pb.bytes"));
+                var session = new TFSession(graph);
             }
         }
     }
