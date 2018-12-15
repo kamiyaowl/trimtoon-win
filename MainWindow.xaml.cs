31d46688479ad20dd410717e62a19fc55c084ad9
K <fieldprogrammable@gmail.com>
2018-12-15 20:05:12 +0900

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TensorFlow;
+
+namespace trimtoon_win
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e) {
+            using (var session = new TFSession()) {
+                var graph = session.Graph;
+
+                var a = graph.Const(2);
+                var b = graph.Const(3);
+                Console.WriteLine("a=2 b=3");
+
+                // Add two constants
+                var addingResults = session.GetRunner().Run(graph.Add(a, b));
+                var addingResultValue = addingResults.GetValue();
+                Console.WriteLine("a+b={0}", addingResultValue);
+
+                // Multiply two constants
+                var multiplyResults = session.GetRunner().Run(graph.Mul(a, b));
+                var multiplyResultValue = multiplyResults.GetValue();
+                Console.WriteLine("a*b={0}", multiplyResultValue);
+            }
+        }
+    }
+}
